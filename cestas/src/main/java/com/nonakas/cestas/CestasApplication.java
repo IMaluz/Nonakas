f50fe2e779a911d96360a9e5958e9c3318e9e669
ImaluZ <maripaiza@gmail.com>
2022-09-14 19:06:13 -0300

    Inciando projeto

---
+++ cestas/src/main/java/com/nonakas/cestas/CestasApplication.java
@@ -0,0 +1,13 @@
+package com.nonakas.cestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CestasApplication.class, args);
+	}
+
+}
