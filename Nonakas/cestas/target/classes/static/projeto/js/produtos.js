bf099b140cee074a70a84e9d166bcb0da6aab73f
Carla S. Nonaka <91844609+carlasnonaka@users.noreply.github.com>
2022-11-03 17:09:37 -0300

    "Ajuste de algumas telas"
    Realizado ajuste nas telas inicial, compras, estoque e relatório

---
+++ Nonakas/cestas/target/classes/static/projeto/js/produtos.js
@@ -0,0 +1,65 @@
+let produtoJson = [
+    {
+        id: 1,
+        name: 'Torrada',
+        img: '../projeto/img/produto1.png',
+        price: 6.00,
+        description: 'Torrada tradicional, pacote com 142g'
+      },
+      {
+        id: 2,
+        name: 'Rosquinhas',
+        img: '../projeto/img/produto2.png',
+        price: 6.00,
+        description: 'Rosquinhas de coco Panco, pacote com 500g'
+      },
+      {
+        id: 3,
+        name: 'Iogurte Natural',
+        img: '../projeto/img/produto3.png',
+        price: 3.00,
+        description: 'Iogurte integral natural, copo com 170g'
+      },
+      {
+        id: 4,
+        name: 'Nutella',
+        img: '../projeto/img/produto4.png',
+        price: 9.00,
+        description: 'Creme de avelã - Nutella, unidade com 140g'
+      },
+      {
+        id: 5,
+        name: 'Suco de maracujá',
+        img: '../projeto/img/produto5.png',
+        price: 4.00,
+        description: 'Suco de maracujá - Maguary, unidade com 1L'
+      },
+      {
+        id: 6,
+        name: 'Cereal Matinal',
+        img: '../projeto/img/produto6.png',
+        price: 46.00,
+        description: 'Cereal sabor chocolate - Nesquik, unidade com 250g'
+      },
+      {
+        id: 7,
+        name: 'Queijo Branco',
+        img: '../projeto/img/produto7.png',
+        price: 37.00,
+        description: 'Queijo Minas frescal - Frimesa, unidade com 400g'
+      },
+      {
+        id: 8,
+        name: 'Chocolate ao Leite',
+        img: '../projeto/img/produto8.png',
+        price: 5.00,
+        description: 'Chocolate ao leite - Lacta, unidade com 90g'
+      },
+      {
+        id: 9,
+        name: 'Pão de Forma',
+        img: '../projeto/img/produto9.png',
+        price: 7.00,
+        description: 'Pão de forma tradicional - Pullman, unidade com 400g'
+      }
+];
