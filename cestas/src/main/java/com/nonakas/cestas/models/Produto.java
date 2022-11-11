f05a9b33cceb569dd1525fc33230d66246e07c9c
Carla S. Nonaka <carlasnonaka@gmail.com>
2022-11-11 16:22:57 -0300

    novasTelas

---
+++ cestas/src/main/java/com/nonakas/cestas/models/Produto.java
@@ -0,0 +1,74 @@
+package com.nonakas.cestas.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Produto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	@NotBlank
+	private String produto;
+	@NotBlank
+	public String precoVenda;
+	@NotBlank
+	private String categoria;
+	@NotBlank
+	private String quantidade;
+
+	private String dataRegistro;
+	
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getProduto() {
+		return produto;
+	}
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+	public String getPrecoVenda() {
+		return precoVenda;
+	}
+	public void setPrecoVenda(String precoVenda) {
+		this.precoVenda = precoVenda;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDataRegistro() {
+		return dataRegistro;
+	}
+	public void setDataRegistro(String dataRegistro) {
+		this.dataRegistro = dataRegistro;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public String getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+}
